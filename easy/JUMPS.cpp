b418718eae93bced46e9cc585d5a305c6d2d24bb
vipul2001 <vbansal@me.iitr.ac.in>
2019-11-30 19:56:58 +0530

    Added More questions

---
+++ easy/JUMPS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ll a;
+    cin>>a;
+    if(a%3==0 || a%6==1)
+    cout<<"yes"<<endl;
+    else
+    cout<<"no"<<endl;
+    return 0;
+}
