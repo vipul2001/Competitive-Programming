1e790e8da0f4078787291e5d0b20186f482b4495
Vipul Bansal <47273021+vipul2001@users.noreply.github.com>
2020-04-02 17:36:48 +0530

    Add files via upload

--- Algorithms/GCD AND LCM/LCM and GCD.cpp
+++ Algorithms/GCD AND LCM/LCM and GCD.cpp
@@ -1,1 +1,31 @@
-
+#include <iostream>
+using namespace std;
+int gcd(int a,int b)
+{
+	if(b==0)
+	{
+		return a;
+	}
+	else
+	{
+		return gcd(b,a%b);
+	}
+}
+int  lcm(int a,int b)
+{
+	return a*b/gcd(a,b);
+}
+int main()
+{
+	int t,a,b;
+	cin>>t;
+	while(t--)
+	{
+		cin>>a;
+		cin>>b;
+		cout<<gcd(a,b)<<endl;
+		cout<<lcm(a,b)<<endl;
+		
+	}
+	return 0;
+}
