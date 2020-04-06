7b9f06768f58fbcd56a7da908bb91654750e6a74
Vipul Bansal <47273021+vipul2001@users.noreply.github.com>
2020-04-06 22:32:50 +0530

    Add files via upload

---
+++ Algorithms/fibonnaci/fibonacci.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+pair<int, int> fib (int n) {
+    if (n == 0)
+        return {0, 1};
+    pair<int,int> p;
+    p = fib(n >> 1);
+    int c = p.first * (2 * p.second - p.first);
+    int d = p.first * p.first + p.second * p.second;
+    if (n & 1)
+        return {d, c + d};
+    else
+        return {c, d};
+}
+int main()
+{
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		pair<int,int> p;
+		p=fib(n);
+		
+		cout<<p.first<<endl<<p.second<<endl;
+	}
+}
